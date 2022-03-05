960c7059c60c0c782638dddfd3a059a8f967a8bf
Mattias Wegblad <mattias.h.wegblad@gmail.com>
2022-03-05 01:35:03 +0100

    module-info.java

---
+++ application/security/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module security {
+    requires spring.context;
+    requires spring.data.relational;
+    requires spring.data.commons;
+    requires spring.session.core;
+    requires reactor.core;
+    requires data;
+    requires spring.boot.autoconfigure;
+    requires spring.web;
+    requires spring.core;
+    requires io.netty.codec.http;
+    requires spring.security.web;
+    requires spring.security.oauth2.client;
+    requires spring.security.config;
+    requires spring.beans;
+}
