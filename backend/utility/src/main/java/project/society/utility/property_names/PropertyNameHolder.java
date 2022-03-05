febeff6f286736463b6c63f98a4a8fa8104dd40d
Mattias Wegblad <mattias.h.wegblad@gmail.com>
2022-03-06 00:43:49 +0100

    documented entire utility module

--- backend/utility/src/main/java/project/society/utility/property_names/PropertyNameHolder.java
+++ backend/utility/src/main/java/project/society/utility/property_names/PropertyNameHolder.java
@@ -1,5 +1,8 @@
 package project.society.utility.property_names;
 
+/**
+ * Contains names of environment variables.
+ */
 public class PropertyNameHolder {
     public static final String PROJECT_DEV_CSRF = "project.dev.csrf";
     public static final String PROJECT_DEV_FRONTEND_URL = "project.dev.frontend.url";
