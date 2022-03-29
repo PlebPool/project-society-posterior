f06eeaa1ecaa05f128a0c0e7fad98f6f29f1c1c8
PlebPool <mattias.h.wegblad@gmail.com>
2022-03-29 20:21:43 +0200

    Added scheme for authorized_clients table

--- backend/application/src/main/resources/schema-example.sql
+++ backend/application/src/main/resources/schema-example.sql
@@ -8,7 +8,7 @@ GRANT ALL ON db_name.* TO 'user'@'%';
 CREATE TABLE IF NOT EXISTS db_name.time_block_day(
     db_id VARCHAR(255) PRIMARY KEY,
     uuid VARCHAR(255), day_of_week VARCHAR(255),
-    time_blocks TEXT
+    time_blocks BLOB # List with time blocks (sorted and indexed by time)
     );
 
 CREATE TABLE IF NOT EXISTS db_name.sessions(
@@ -18,6 +18,12 @@ CREATE TABLE IF NOT EXISTS db_name.sessions(
     creation_time DATETIME NOT NULL,
     last_accessed_time DATETIME NOT NULL,
     expires_at DATETIME NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS db_name.authorized_clients(
+    principal_name VARCHAR(255) PRIMARY KEY,
+    client_id VARCHAR(255) NOT NULL,
+    body BLOB
     );
 
 CREATE EVENT db_name.delete_expired ON SCHEDULE EVERY 1 MINUTE DO DELETE FROM db_name.sessions WHERE expires_at < NOW();
