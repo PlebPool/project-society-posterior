8392e6f33fb7776e0ab5d26691cefbce47cfbc20
PlebPool <mattias.h.wegblad@gmail.com>
2022-03-29 20:23:23 +0200

    Created Bean configuration for authorized client security package

--- backend/security/src/main/java/project/society/security/authorizedclient/config/AuthorizedClientConfig.java
+++ backend/security/src/main/java/project/society/security/authorizedclient/config/AuthorizedClientConfig.java
@@ -3,7 +3,6 @@ package project.society.security.authorizedclient.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import project.society.data.dao.GenericReactiveDAO;
 import project.society.security.authorizedclient.MySqlReactiveOAuth2AuthorizedClientService;
 import project.society.security.authorizedclient.model.OAuth2ClientDAOService;
@@ -11,12 +10,13 @@ import project.society.security.authorizedclient.model.OAuth2ClientDAOService;
 @Configuration
 public class AuthorizedClientConfig {
 
-    private OAuth2ClientDAOService oAuth2ClientDAOService(GenericReactiveDAO dao) {
+    @Bean
+    public OAuth2ClientDAOService oAuth2ClientDAOService(GenericReactiveDAO dao) {
         return new OAuth2ClientDAOService(dao);
     }
 
     @Bean
-    public ReactiveOAuth2AuthorizedClientService serverOAuth2AuthorizedClientRepository(GenericReactiveDAO dao) {
-        return new MySqlReactiveOAuth2AuthorizedClientService(this.oAuth2ClientDAOService(dao));
+    public ReactiveOAuth2AuthorizedClientService serverOAuth2AuthorizedClientRepository(OAuth2ClientDAOService dao) {
+        return new MySqlReactiveOAuth2AuthorizedClientService(dao);
     }
 }
