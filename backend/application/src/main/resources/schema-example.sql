8e19602ca629d272a92a5c3801b826d4d01ffefd
Mattias Wegblad <mattias.h.wegblad@gmail.com>
2022-04-01 15:05:49 +0200

    updated sql

--- backend/application/src/main/resources/schema-example.sql
+++ backend/application/src/main/resources/schema-example.sql
@@ -1,17 +1,17 @@
-DROP DATABASE IF EXISTS db_name;
-DROP USER IF EXISTS 'user'@'%';
-DROP EVENT IF EXISTS db_name.delete_expired;
-CREATE DATABASE IF NOT EXISTS db_name;
-CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'pass';
-GRANT ALL ON db_name.* TO 'user'@'%';
+DROP DATABASE IF EXISTS project_society;
+DROP USER IF EXISTS 'society'@'%';
+DROP EVENT IF EXISTS project_society.delete_expired;
+CREATE DATABASE IF NOT EXISTS project_society;
+CREATE USER IF NOT EXISTS 'society'@'%' IDENTIFIED BY 'ThePassword';
+GRANT ALL ON project_society.* TO 'society'@'%';
 
-CREATE TABLE IF NOT EXISTS db_name.time_block_day(
+CREATE TABLE IF NOT EXISTS project_society.time_block_day(
     db_id VARCHAR(255) PRIMARY KEY,
     uuid VARCHAR(255), day_of_week VARCHAR(255),
     time_blocks BLOB # List with time blocks (sorted and indexed by time)
     );
 
-CREATE TABLE IF NOT EXISTS db_name.sessions(
+CREATE TABLE IF NOT EXISTS project_society.sessions(
     id VARCHAR(255) PRIMARY KEY,
     original_id VARCHAR(255) NOT NULL,
     session_attrs BLOB,
@@ -20,10 +20,17 @@ CREATE TABLE IF NOT EXISTS db_name.sessions(
     expires_at DATETIME NOT NULL
     );
 
-CREATE TABLE IF NOT EXISTS db_name.authorized_clients(
+CREATE TABLE IF NOT EXISTS project_society.authorized_clients(
     principal_name VARCHAR(255) PRIMARY KEY,
     client_id VARCHAR(255) NOT NULL,
     body BLOB
     );
 
-CREATE EVENT db_name.delete_expired ON SCHEDULE EVERY 1 MINUTE DO DELETE FROM db_name.sessions WHERE expires_at < NOW();
+CREATE TABLE IF NOT EXISTS project_society.time_block(
+    uuid VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    day_of_week INTEGER NOT NULL,
+    time_block_list BLOB
+    );
+
+CREATE EVENT project_society.delete_expired ON SCHEDULE EVERY 1 MINUTE DO DELETE FROM project_society.sessions WHERE expires_at < NOW();
