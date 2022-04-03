96674186a3210ff85c5f291b2eff37c2453f4516
PlebPool <mattias.h.wegblad@gmail.com>
2022-04-04 00:03:00 +0200

    allowing http response reason messages

--- backend/application/src/main/resources/schema-example.sql
+++ backend/application/src/main/resources/schema-example.sql
@@ -5,11 +5,12 @@ CREATE DATABASE IF NOT EXISTS project_society;
 CREATE USER IF NOT EXISTS 'society'@'%' IDENTIFIED BY 'ThePassword';
 GRANT ALL ON project_society.* TO 'society'@'%';
 
-CREATE TABLE IF NOT EXISTS project_society.time_block_day(
-    db_id VARCHAR(255) PRIMARY KEY,
-    uuid VARCHAR(255), day_of_week VARCHAR(255),
-    time_blocks BLOB # List with time blocks (sorted and indexed by time)
-    );
+CREATE TABLE IF NOT EXISTS project_society.time_block_days(
+         uuid VARCHAR(255) PRIMARY KEY,
+         user_id VARCHAR(255) NOT NULL,
+         day_of_week INTEGER NOT NULL,
+         time_block_list BLOB
+);
 
 CREATE TABLE IF NOT EXISTS project_society.sessions(
     id VARCHAR(255) PRIMARY KEY,
@@ -18,19 +19,6 @@ CREATE TABLE IF NOT EXISTS project_society.sessions(
     creation_time DATETIME NOT NULL,
     last_accessed_time DATETIME NOT NULL,
     expires_at DATETIME NOT NULL
-    );
-
-CREATE TABLE IF NOT EXISTS project_society.authorized_clients(
-    principal_name VARCHAR(255) PRIMARY KEY,
-    client_id VARCHAR(255) NOT NULL,
-    body BLOB
-    );
-
-CREATE TABLE IF NOT EXISTS project_society.time_block(
-    uuid VARCHAR(255) PRIMARY KEY,
-    user_id VARCHAR(255) NOT NULL,
-    day_of_week INTEGER NOT NULL,
-    time_block_list BLOB
     );
 
 CREATE EVENT project_society.delete_expired ON SCHEDULE EVERY 1 MINUTE DO DELETE FROM project_society.sessions WHERE expires_at < NOW();
