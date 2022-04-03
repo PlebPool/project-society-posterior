320b10e39cc1c2abd692ac029a0d69042cbfdbb9
PlebPool <mattias.h.wegblad@gmail.com>
2022-04-04 00:06:45 +0200

    created http 400 response status exception

---
+++ backend/web/src/main/java/project/society/web/timeblocks/exceptions/DayIndexOutOfBoundsException.java
@@ -0,0 +1,9 @@
+package project.society.web.timeblocks.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Day index out of bounds.")
+public class DayIndexOutOfBoundsException extends RuntimeException {
+
+}
