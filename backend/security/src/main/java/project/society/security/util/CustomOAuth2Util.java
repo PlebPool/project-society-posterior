6fb37f4e70ff81300b8c1176b61fede0709e5dd0
PlebPool <mattias.h.wegblad@gmail.com>
2022-04-04 00:05:41 +0200

    extracted principal name and access token getters into new methods. Hiding OAuth2AuthorizedClient

--- backend/security/src/main/java/project/society/security/util/CustomOAuth2Util.java
+++ backend/security/src/main/java/project/society/security/util/CustomOAuth2Util.java
@@ -3,13 +3,13 @@ package project.society.security.util;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import reactor.core.publisher.Mono;
 
 @Component
-public class
-CustomOAuth2Util {
+public class CustomOAuth2Util {
     private final ServerOAuth2AuthorizedClientRepository auth2AuthorizedClientRepository;
 
     public CustomOAuth2Util(ServerOAuth2AuthorizedClientRepository auth2AuthorizedClientRepository) {
@@ -22,13 +22,23 @@ CustomOAuth2Util {
      * @return {@link Mono} of {@link String}.
      */
     public Mono<String> extractAccessToken(ServerRequest request) {
-        Mono<OAuth2AuthorizedClient> oAuth2AuthorizedClient = request.principal().filter(principal -> principal instanceof OAuth2AuthenticationToken)
+        return this.extractAuthorizedClient(request)
+                .map(OAuth2AuthorizedClient::getAccessToken)
+                .map(OAuth2AccessToken::getTokenValue);
+    }
+
+    public Mono<String> extractPrincipalName(ServerRequest request) {
+        return this.extractAuthorizedClient(request)
+                .map(OAuth2AuthorizedClient::getPrincipalName);
+    }
+
+    private Mono<OAuth2AuthorizedClient> extractAuthorizedClient(ServerRequest request) {
+        return request.principal().filter(principal -> principal instanceof OAuth2AuthenticationToken)
                 .cast(OAuth2AuthenticationToken.class)
                 .flatMap(oAuth2AuthenticationToken -> auth2AuthorizedClientRepository.loadAuthorizedClient(
                         oAuth2AuthenticationToken.getAuthorizedClientRegistrationId(),
                         oAuth2AuthenticationToken,
                         request.exchange()
                 ));
-        return oAuth2AuthorizedClient.map(oAuth2AuthorizedClient1 -> oAuth2AuthorizedClient1.getAccessToken().getTokenValue());
     }
 }
